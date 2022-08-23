d0261e86813930777360c73cf2703dcb9642d907
Eva-Renata <all2evi@yahoo.com>
2022-08-23 10:31:44 +0200

    style topheader færdig

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import styles from "./App.module.scss";
+import { Header } from "./Components/Header/Header";
 
 function App() {
-  return <div className={styles.Appwrapper}></div>;
+  return (
+    <div className={styles.Appwrapper}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,31 @@
+import logo from "../../images/header-bg.png";
+import styles from "./Header.module.scss";
+import { BsArrowRightSquare } from "react-icons/bs";
+import kurv from "../../images/cart-icon.png";
+import mail from "../../images/mail-icon.png";
+import phone from "../../images/phone-icon.png";
+//import { Link } from "react-router-dom";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section className={styles.topwrapper}>
+      <img src={logo} alt="logo" className={styles.logoimage} />
+      <div className={styles.toplinks}>
+        <a className={styles.kontaktlinks}>Forside</a>
+        <a className={styles.kontaktlinks}>Salgs- og handelsbetingelser</a>
+        <button className={styles.loginbutton}>Login</button>
+      </div>
+      <div className={styles.topright}>
+        <div className={styles.kontakt}>
+          <img src={mail} alt="mail" />
+          <span>SALES@STRINGSONLINE.COM</span>
+          <img src={phone} alt="phone" />
+          <span>+45 98 12 22 68</span>
+          <img src={kurv} alt="kurv" />
+        </div>
+        <input type="text" placeholder={"Indtast søgeord"} />
+        <BsArrowRightSquare />
+      </div>
+    </section>
+  );
 };
